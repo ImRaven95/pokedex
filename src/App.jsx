40a9050d68dd7594cebeb4ba0bd390a3dcede943
Elias Ojala <elkkuve@gmail.com>
2023-02-16 14:26:17 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar';
 import PokemonGrid from './components/PokemonGrid';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <PokemonGrid />
+      <Footer />
     </div>
   );
 }
--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -4,6 +4,9 @@ import "./PokemonGrid.css";
 
 function PokemonGrid() {
   const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   //  Next make an HTTP GET request to the PokeAPI endpoint /api/v2/pokemon, which returns a list of all the Pokemons.
   useEffect(() => {
@@ -19,12 +22,31 @@ function PokemonGrid() {
   return (
     <div className="pokemon-grid">
       {pokemons.map(pokemon => (
-        <div className="pokemon-card" key={pokemon.name}>
+        <div className="pokemon-card" key={pokemon.name} onClick={() => setSelectedPokemon(pokemon)}>
           <img className="pokemon-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} alt={pokemon.name} />
           <div className="pokemon-name">{pokemon.name}</div>
         </div>
       ))}
-    </div>
+    {selectedPokemon && (
+      <div className="pokemon-modal">
+        <div className="pokemon-modal-content">
+          <img className="pokemon-modal-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.url.split('/')[6]}.png`} alt={selectedPokemon.name} />
+          <div className="pokemon-modal-name">{selectedPokemon.name}</div>
+          <div className="pokemon-modal-details">
+            <p>Type: </p>
+            {(()=>{console.log(pokemons.types);return <p></p>})()}
+            {pokemons.types.map((type) => (
+              <p key={type.slot}>{type.type.name}</p>
+            ))}
+            
+            <p>Height: {selectedPokemon.height}</p>
+            <p>Weight: {selectedPokemon.weight}</p>
+          </div>
+          <button onClick={() => setSelectedPokemon(null)}>Close</button>
+        </div>
+      </div>
+    )}
+    </div> 
   )
 }
 
